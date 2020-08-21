bd2c5ea340fc06ab0bb3bfd2470959b6a4877de2
Mark <agranatmarkit@gmail.com>
2020-08-21 22:19:58 +0300

    Initial Commit

---
+++ CardsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardsApp
+//
+//  Created by Марк on 21.08.2020.
+//  Copyright © 2020 Agranat Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
